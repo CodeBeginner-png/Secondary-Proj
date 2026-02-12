77750718082192da2e8c3d10fcdb1085e9e6d87d
Christian <cawilliams03@batestech.edu>
2026-02-11 17:29:28 -0800

    Fixed cars and arraylist errors

--- src/CollegeProjects/ArraysArrayList.java
+++ src/CollegeProjects/ArraysArrayList.java
@@ -19,6 +19,7 @@ public class ArraysArrayList {
             list.add(3);
             list.add(4);
             list.add(5);
+            
 
             System.out.println("Size of the ArrayList: " + list.size());
             
@@ -37,7 +38,7 @@ public class ArraysArrayList {
             ArrayList<String> names = new ArrayList<>();
             names.add("Alice");
             names.add("Bob");
-            names.add(123); // This will cause a compile-time error because the list is of type String
+            list.add(123);; // This will cause a compile-time error because the list is of type String
             names.add("Charlie");
 
             for (String x : names) {
